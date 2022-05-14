7ba1d7ad8f2911ab7dcf301b51b0b68cb84d4c3d
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-05-13 17:07:14 -0700

    @ home submit

--- server.py
+++ server.py
@@ -3,16 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!!'
+
+
+# def level_1():
+#     return render_template("index.html")
 
 
 @app.route('/play')
-def level_1():
-    return render_template("index.html")
-
-
 @app.route('/play/<int:x>/<string:color>')
-def level_2(x, color):
+def level_2(x=3, color='aquamarine'):
     return render_template("level_2.html", x=x, color=color)
 
 if __name__ == "__main__":
